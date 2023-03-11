b605635fc6bd3144837bbdf327da146c77b79ea8
건 초이 <105775683+na0th@users.noreply.github.com>
2023-03-11 21:27:08 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -5,11 +5,12 @@ def solution(clothes):
     for clothes_set in clothes:
         dic[clothes_set[1]] += 1
         
-    print(dic)
+    
     list1 = []
     
     for key,value in dic.items() :
         list1.append(value)
+        
     res = 1    
     for num in list1:
        res = res*(num+1) 
