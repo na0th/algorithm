128dde3ba6c285c6fcd5de639d831da517520ac7
건 초이 <105775683+na0th@users.noreply.github.com>
2023-03-11 19:44:43 +0900

    [level 2] Title: 전화번호 목록, Time: 443.76 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,29 +1,15 @@
 def solution(phone_book):
 
-
-
-#     아래의 함수 startswith 문자열 겹치는 거 체크하는 거 기억하기
-#         if phone_book[i+1].startswith(phone_book[i]):
-
-    
-    # phone_book.sort(key = lambda x:x[0]) 이거는 안된다..
-    # phone_book.sort()는 된다..     
-    
-#  정렬을 해놓으면 굳이 끝까지 뒤지지 않고 내 옆에만 비교하면 된다
-#  ex 111,1189,235 이런 식으로 이미 아닌 게 나왔으면 앞으로 쭉 아니다 
-    phone_book.sort()
-    
-    for i in range(len(phone_book)-1):
-        count = 0
-        str_len = len(phone_book[i])
-        for j in range(str_len):
-            if len(phone_book[i]) < len(phone_book[i+1]):
-                if phone_book[i][j] == phone_book[i+1][j] :
-                    count+=1
-                else:
-                    break
-        if count == str_len :
-            return False
-    
     answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
     return answer
+
+    
