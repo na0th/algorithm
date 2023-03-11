31ab0ec89e98a1f1682054c99f416101f31a277b
건 초이 <105775683+na0th@users.noreply.github.com>
2023-03-11 16:34:01 +0900

    Update 전화번호 목록.py

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,16 @@
 def solution(phone_book):
-    
-#     phone_book.sort()
 
-#     for i in range(len(phone_book)-1):
+
+
+#     아래의 함수 startswith 문자열 겹치는 거 체크하는 거 기억하기
 #         if phone_book[i+1].startswith(phone_book[i]):
-#             return False
 
-#     return True
     
-    # phone_book.sort(key = lambda x:x[0])
+    # phone_book.sort(key = lambda x:x[0]) 이거는 안된다..
+    # phone_book.sort()는 된다..     
+    
+#  정렬을 해놓으면 굳이 끝까지 뒤지지 않고 내 옆에만 비교하면 된다
+#  ex 111,1189,235 이런 식으로 이미 아닌 게 나왔으면 앞으로 쭉 아니다 
     phone_book.sort()
     
     for i in range(len(phone_book)-1):
