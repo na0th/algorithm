5a2e04f7d4d6965f21c3fd5c1ea02096d2368ea8
건 초이 <105775683+na0th@users.noreply.github.com>
2023-03-11 21:26:35 +0900

    [level 2] Title: 위장, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -0,0 +1,20 @@
+def solution(clothes):
+    from collections import defaultdict
+    dic = defaultdict(int)       
+    
+    for clothes_set in clothes:
+        dic[clothes_set[1]] += 1
+        
+    print(dic)
+    list1 = []
+    
+    for key,value in dic.items() :
+        list1.append(value)
+    res = 1    
+    for num in list1:
+       res = res*(num+1) 
+    
+    
+    
+    answer=res-1
+    return answer
