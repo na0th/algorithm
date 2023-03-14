33bd9753d37824325f0b61546c314ec6fd0dbd31
건 초이 <105775683+na0th@users.noreply.github.com>
2023-03-14 20:04:46 +0900

    [level 2] Title: 올바른 괄호, Time: 19.69 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,19 @@
 def solution(s):
     from collections import deque
-    
-    answer = False
-    
+#   데크 
     deq = deque()
     for i in range(len(s)):
+#       홀수면 보지도 않고 틀렸다.
         if len(s) % 2 == 1 :
             return False
+#       스택으로 "(" 나오면 push
         if s[i] == "(" :
             deq.append("(")
+#       스택으로 ")" 나오면 스택에서 제거 (없으면 안하도록 예외처리)
         elif "(" in deq and s[i] == ")":
             deq.remove("(")
         
-    
+#   그래서 쌍이 잘 맞아서 전부 지워졌으면 True 아니면 False 리턴
     if not deq :
         return True
     if deq :
