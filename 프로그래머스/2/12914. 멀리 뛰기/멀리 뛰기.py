e1744634ef14f0c7e6effe66fd924ced71673120
건 초이 <105775683+na0th@users.noreply.github.com>
2024-01-19 23:39:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.57 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    dp ={1:1,2:2}
+    
+    if n not in dp :
+        for i in range(3,n+1):        
+            dp[i] = dp[i-1]+dp[i-2]
+
+            
+    return dp[n]%(1234567)
+            
