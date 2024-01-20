ec452a020303086c20d9c973735c21458c1e3f9b
건 초이 <105775683+na0th@users.noreply.github.com>
2024-01-20 20:04:32 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,13 +1,3 @@
 def solution(s):
-    capital=[]
-#   'A' = 65
-#   'a' = 97
-    
-    print(ord('a'))
-    
-    s = list(s)
-    print(s)
-    answer = sorted(s,key=lambda x:ord(x), reverse=True)
-    print(answer)
-    answer = ''.join(answer)
+    answer = ''
     return answer
