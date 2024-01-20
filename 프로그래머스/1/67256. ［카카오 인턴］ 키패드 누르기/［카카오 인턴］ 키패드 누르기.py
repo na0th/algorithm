ef6a2fad1efa284b56f9163d142f5e3569574c33
건 초이 <105775683+na0th@users.noreply.github.com>
2024-01-21 00:14:17 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,68 @@
+def solution(numbers, hand):
+#   왼손 위치 변수 '' , 오른손 위치 변수 ""
+#  눌러야 할 손까지 거리를 구한다. 같으면 왼손,오른손잡이 손 따라서..
+
+    dic = dict()
+    dic['1']=[1,1]
+    dic['2']=[1,2]
+    dic['3']=[1,3]
+    dic['4']=[2,1]
+    dic['5']=[2,2]
+    dic['6']=[2,3]
+    dic['7']=[3,1]
+    dic['8']=[3,2]
+    dic['9']=[3,3]
+    dic['*']=[4,1]
+    dic['0']=[4,2]
+    dic['#']=[4,3]
+    
+    now_left='*'
+    now_right='#'
+    answer = []
+    for number in numbers:
+        print("number")
+        if number in [1,4,7] :
+            answer.append("L")
+            print(1)
+            now_left = str(number)
+            continue
+        if number in [3,6,9] :
+            answer.append("R")
+            print(2)
+            now_right = str(number)
+            continue
+        else :
+            number = str(number)
+            left_distance_x = abs(dic[now_left][0]-dic[number][0])
+            left_distance_y = abs(dic[now_left][1]-dic[number][1])
+
+            right_distance_x = abs(dic[now_right][0]-dic[number][0])
+            right_distance_y = abs(dic[now_right][1]-dic[number][1])
+
+            left_sum_distance = left_distance_x +left_distance_y
+            right_sum_distance = right_distance_x+right_distance_y
+        
+            if left_sum_distance > right_sum_distance:
+                answer.append("R")
+                print(3)
+                now_right = number
+                continue
+            if left_sum_distance < right_sum_distance:
+                answer.append("L")
+                print(4)
+                now_left = number
+                continue
+            else :
+                if hand == "right":
+                    answer.append("R")
+                    print(5)
+                    now_right = number
+                else :
+                    answer.append("L")
+                    print(6)
+                    now_left = number
+
+    
+            
+    answer = ''.join(answer)
+    return answer
