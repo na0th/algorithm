6985d868751c741e041d3d6e27110c86d8922196
건 초이 <105775683+na0th@users.noreply.github.com>
2024-01-22 20:38:39 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,5 +1,4 @@
 def solution(num, total):
-    num_list = [x for x in range(0,101)]
 
 #    num , total  a~b 
 #    홀 홀     중앙값 +- num -1 // 2  n n+1 n+2 / 3n+3  5n+10
@@ -14,18 +13,7 @@ def solution(num, total):
 #    4  10  [1,2,3,4]
 #    8  36   1234 4.5 5678
     middle_num = (total//num)
-    if num % 2 == 1 and total % 2 == 1 :
+    if num % 2 == 1 :
         return [x for x in range(middle_num-((num//2)),middle_num+((num//2)+1))]
-        # return num_list[middle_num-((num//2)):middle_num+((num//2)+1)]
-    elif num % 2 == 1 and total % 2 == 0 :
-        return [x for x in range(middle_num-((num//2)),middle_num+((num//2)+1))]
-        # return num_list[middle_num-((num//2)):middle_num+((num//2)+1)]
-    elif num % 2 == 0 and total % 2 == 1 :
-        return [x for x in range(middle_num-(num//2)+1,middle_num+num//2+1)]
-        # return num_list[middle_num-(num//2)+1:middle_num+num//2+1]
     else :
         return [x for x in range(middle_num-(num//2)+1,middle_num+num//2+1)]
-        # return num_list[middle_num-(num//2)+1:middle_num+num//2+1]
-        
-    answer = []
-    return answer
