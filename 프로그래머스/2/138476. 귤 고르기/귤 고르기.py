0390088bbfd88c030e2c54d5e88b2340542648c0
건 초이 <105775683+na0th@users.noreply.github.com>
2024-01-25 13:47:16 +0900

    [level 2] Title: 귤 고르기, Time: 19.52 ms, Memory: 19.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,36 @@
+def solution(k, tangerine):
+#    수확 귤중 k개를 골라 판매
+#     서로다른 종류를 최소화..
+    # counter해서 앞에서 부터 출력하면 되지 않을까..?
+    
+    from collections import Counter
+    
+    dic = dict(Counter(tangerine))
+    # print(dic)
+#   key로 정렬  
+    # dic = dict(sorted(dic.items()))
+    # print(dic)
+    
+#   value로 정렬
+    # dic = dict(sorted(dic.items(),key=lambda x:x[1],reverse=True))
+    
+    value_list = list(dic.values())
+    value_list.sort(reverse=True)
+    # print(value_list)
+    
+    total = 0
+    cnt = 0
+    for value in value_list:
+        if total >= k :
+            break
+        else :
+            total += value
+            cnt += 1
+            
+        # print(total,cnt)
+
+
+      
+#             
+        # 2 2 2
+    return cnt
