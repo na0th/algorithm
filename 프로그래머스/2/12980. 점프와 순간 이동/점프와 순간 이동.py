29f4a5664421acada4172ccee2ef0eca9768a70b
건 초이 <105775683+na0th@users.noreply.github.com>
2024-01-30 13:12:14 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,12 +1,5 @@
 def solution(n):
-    ans = 0
-#   k칸 점프 or 현재까지 온 거리 2배 순간이동
-#   점프를 최소화하고 2배 순간이동을 많이 써라
 
-    ans = bin(n)[2:]
-    count = 0
-    for i in range(len(ans)) :
-        if ans[i] == '1' :
-            count+=1
-    
-    return count
+#   k칸 점프 or 현재까지 온 거리 2배 순간이동
+#   점프를 최소화하고 2배 순간이동을 많이 써라 
+    return bin(n)[2:].count('1')
