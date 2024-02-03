67835b2320337faa1ebf69f15e6c7fd46d0550c9
건 초이 <105775683+na0th@users.noreply.github.com>
2024-02-03 13:35:27 +0900

    [level 2] Title: 문자열 압축, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,5 +1,11 @@
 def solution(s):
+#   완전탐색으로 처음부터 전부 뒤져서 min 값 리턴하기
+#    1개로 자르기, 2개로 자르기.... n개로 자르기
+
+#   [ [1,'abc'],[2,'bbb'] ]  이런 형태로 추가하기
+#    [-1][1]이 있는지 체크해서 있으면 [-1][0]에 +1
     answer = []
+    
     for k in range(1,len(s)+1):
         cnt_list = []
         cnt = 0
@@ -21,6 +27,7 @@ def solution(s):
         # print()
         
         for q in range(len(cnt_list)):
+#       카운팅 숫자가 2일 때는 1개 자리수 차지, 20일때는 2개 자리수 차지..
             if cnt_list[q][0] != 1 :
                 cnt += len(str(cnt_list[q][0]))
             cnt += len(cnt_list[q][1])
