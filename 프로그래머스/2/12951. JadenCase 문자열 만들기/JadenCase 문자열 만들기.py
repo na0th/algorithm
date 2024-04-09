1f5fa6c4f2bd5cae35c286d054097e103f760fae
건 초이 <105775683+na0th@users.noreply.github.com>
2024-04-09 18:08:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,31 @@
+def solution(s):
+#   공백을 기준으로 자르기+전체 소문자로 만들고+맨앞글자가 알파벳이면 대문자로..
+    # s="  HI  HELLO"
+    s = s.split(" ")
+    s = [item.lower() for item in s]
+    print(s)
+    str = ""
+    for item in s :
+        if item =="":
+            str+=" "
+        else :
+            str+=" "+item[0].upper()+item[1:]
+    str = list(str)
+    del str[0]
+    a=""
+    for item in str :
+        if item =="":
+            a+=" "
+        else :
+            a+=item
+    return a
+#     for item in s:
+#         if item[0].isalpha():
+#             str = item[0].upper()+item[1:]
+#             list.append(str)
+#             continue
+#         list.append(item)
+        
+#     print(list)
+#     answer = " ".join(list)
+#     return answer
