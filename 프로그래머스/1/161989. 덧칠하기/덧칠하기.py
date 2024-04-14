6b4b904cecab8dce415b79c1b1dbe4e1f7fe19b4
건 초이 <105775683+na0th@users.noreply.github.com>
2024-04-14 17:35:47 +0900

    [level 1] Title: 덧칠하기, Time: 5.40 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,23 +1,12 @@
 def solution(n, m, section):
-    section.sort(reverse=True)
     
+    section.sort(reverse=True)
     cnt = 0
-    # while(section):
-    #     num = section.pop(0)
-    #     while(section and section[-1] < num+m) :
-    #         if section :
-    #             section.pop()
-    #             if not section:
-    #                 break
-    #     cnt+=1
+    
     while(section):
         num = section.pop()
         while(section and section[-1] < num+m):
             section.pop()
         cnt+=1
         
-        
-        
-            
-    answer = cnt
-    return answer
+    return cnt
