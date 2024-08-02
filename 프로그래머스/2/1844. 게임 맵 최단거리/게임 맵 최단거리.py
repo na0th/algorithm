ca37d2a56249a31be3e5c7905ab877cd9cb6a62c
건 초이 <105775683+na0th@users.noreply.github.com>
2024-08-02 22:42:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.29 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,35 @@
+from collections import deque
+def solution(maps):
+#     최단거리는 BFS라 했는데..
+#     BFS는 Queue, Linked List로 구현한다 했었고
+    
+    start = (0,0)
+    def BFS(maps,start):
+        rows, cols = len(maps), len(maps[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        queue = deque([start])
+        visited = set()
+        visited.add(start)
+        
+        distance = [[-1] * cols for _ in range(rows)]
+        distance[0][0] = 1
+        
+        while(queue):
+            current = queue.popleft()
+            x, y = current
+            
+            if x == rows-1 and y == cols-1:
+                # print(distance)
+                return distance[x][y]
+                
+            for direction in directions:
+                nx, ny = x + direction[0], y + direction[1]
+                if 0 <= nx < rows and 0 <= ny < cols:
+                    if maps[nx][ny] == 1 and (nx, ny) not in visited:
+                        visited.add((nx, ny))
+                        queue.append((nx, ny))
+                        distance[nx][ny] = distance[x][y] + 1
+        return -1
+    
+    visited_positions = BFS(maps, start)
+    return visited_positions
