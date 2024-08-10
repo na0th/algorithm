4d01b19899a206db5fbb5c89bda17b4b484adad8
건 초이 <105775683+na0th@users.noreply.github.com>
2024-08-11 00:17:34 +0900

    [level 3] Title: 정수 삼각형, Time: 50.39 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -8,21 +8,13 @@ def solution(triangle):
     
     '''
     for k,item in enumerate(triangle) :
-        # print(item)
         for i in range(len(item)) :
             if k > 0 :
                 if i == 0 :
-                    # print("k=",k,"triangle",triangle[k-1][0])
                     item[i] = triangle[k-1][0] + item[i]
-                    # print(item[i],end=' ')
                 elif i == len(item)-1 :
                     item[i] = triangle[k-1][i-1] + item[i]
-                    # print(item[i],end=' ')
                 else : 
                     item[i] = max(triangle[k-1][i-1],triangle[k-1][i]) +item[i]
-                    # print(item[i],end=' ')
-    
-                
-        # print()
-    # print(triangle)
+
     return max(triangle[-1])
