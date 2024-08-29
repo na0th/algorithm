cc27c79db79c875c04b31e6741fc111bccd9df22
건 초이 <105775683+na0th@users.noreply.github.com>
2024-08-29 23:10:51 +0900

    [level 3] Title: 가장 먼 노드, Time: 5373.16 ms, Memory: 23.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,32 @@
+def solution(n, edge):
+    from collections import defaultdict
+    from collections import deque
+    '''
+    bfs 구현?
+    '''
+    dic = defaultdict(list)
+    for v in edge :
+        dic[v[0]].append(v[1])
+        dic[v[1]].append(v[0])
+        
+    # print(dic)
+    
+    def bfs(start_node, graph):
+        a_list = [1]
+        queue = deque([(start_node,1)])
+        visited = set([start_node])
+        
+        while queue:
+            curr_node, cnt = queue.popleft()
+
+            for next_node in graph[curr_node]:
+                if next_node not in visited:
+                    visited.add(next_node)
+                    queue.append((next_node,cnt+1))
+                    a_list.append(cnt+1)
+            max_value = max(a_list)
+            
+        return a_list.count(max_value)
+            
+    
+    return bfs(1,dic)
