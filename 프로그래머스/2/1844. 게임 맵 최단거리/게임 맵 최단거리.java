f7a3ad4f3a7813b8c8e57c43f1a95a8f8e758f6b
건 초이 <105775683+na0th@users.noreply.github.com>
2025-04-15 22:58:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.53 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -7,32 +7,36 @@ class Solution {
     
     알고리즘 분류 : BFS
     어떻게 풀이 ? 
+    
     [i,j,cnt] 꼴로 정의해서 i,j까지 몇칸이라는 걸 의미하고
     그 상태에서 동서남북을 bfs 큐에 넣고 계속 pop해본다.
     n,m이 나왔을 때의 cnt를 출력하고, queue 다 끝났는데, 없었으면 -1 리턴
+    
+    추가 
+    deque를 List<Integer> 가 아니고 int[]로 만든다. 
+    deque.add(new int[] {} ) <= 이 꼴로 추가하면 됨.
     */
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
             
-        Deque<List<Integer>> deque = new ArrayDeque<>();
+        Deque<int[]> deque = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-
+        
         
         int[] dx = {-1,1,0,0};
         int[] dy = {0,0,1,-1};
         
         int startX =0, startY =0; 
 
-        deque.addLast(Arrays.asList(startX,startY,1));
         visited[0][0]=true;
+        deque.addLast(new int[]{startX,startY,1});
         
         while(!deque.isEmpty()){
-            List<Integer> pop = deque.removeFirst();
-            int x = pop.get(0);
-            int y = pop.get(1);
-            int cnt = pop.get(2);
-            
+            int[] pop = deque.removeFirst();
+            int x = pop[0];
+            int y = pop[1];
+            int cnt = pop[2];
             if (x == n - 1 && y == m - 1) {
                 return cnt;
             }
@@ -41,14 +45,16 @@ class Solution {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx>=0 && nx < n && ny>=0 && ny < m  && maps[nx][ny] == 1 && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    deque.addLast(Arrays.asList(nx, ny, cnt + 1));
+                if(nx>=0 && nx < n && ny>=0 && ny < m){
+                    if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        deque.addLast(new int[]{nx,ny,cnt+1});
                     }
                 }
+                
             
-            
-            
+            }
+        
         }
         return -1;
     }
