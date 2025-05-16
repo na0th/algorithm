a15b0e83e8a9bfa5828b295aec26b2be486fcf78
건 초이 <105775683+na0th@users.noreply.github.com>
2025-05-17 01:31:23 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    /*
+    서로 다른 옷의 조합의 수 구하기
+    
+    알고리즘 분류 : 해시 맵
+    
+    어떻게 풀이?
+    최소 1개의 의상은 입는다.
+    1. map에 종류 갯수를 센다
+    2. map에 있는 종류 갯수 전부 다 +1 해서 곱하고 -1 (의상을 아예 안 입는 경우는 없음)
+    */
+    public int solution(String[][] clothes) {
+        Map<String, Integer> clothMap = new HashMap<>();
+        
+        for(String[] cloth : clothes){
+            if(!clothMap.containsKey(cloth[1])){
+                clothMap.put(cloth[1],1);
+            }
+            else{
+                clothMap.put(cloth[1],clothMap.get(cloth[1])+1);
+            }
+        }
+        int answer = 1;
+        for (Integer value : clothMap.values()) {
+            answer*=(value+1);
+        }
+
+        
+        return answer-1;
+    }
+}
