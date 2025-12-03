b82c1174dd22c5468e03a93b85ecdaedec97657f
건 초이 <105775683+na0th@users.noreply.github.com>
2025-12-03 22:19:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.55 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,31 +1,27 @@
 import java.util.*;
 class Solution {
+    /*
+    분류 : 해시 
+    고려할 점  : 동명이인이 있다. A가 2명있으면 1명만 들어왔으면 A가 완주하지 못한 것
+    어떻게 풀이? 
+    1. participant를 해시에 등록
+    2. completion을 순회하면서 해시에 등록됐는지 확인
     
+    */
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        
-        for(String comp : completion){
-            map.put(comp,map.getOrDefault(comp,0)+1);
-        }
-        // System.out.println(map.toString());
-        
         String answer = "";
+        Map<String, Integer> map = new HashMap<>();
         for(String parti : participant){
-            if(map.containsKey(parti)){
-                map.put(parti,map.get(parti)-1);
-            }
-            else{
-                answer = parti;
-            }
+            map.put(parti, map.getOrDefault(parti,0)+1);
         }
-        
-        
-        for(Map.Entry<String,Integer> entry : map.entrySet()){
-            if(entry.getValue()==-1){
+        for(String compl : completion){
+            map.put(compl, map.get(compl)-1);
+        }
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1){
                 answer = entry.getKey();
             }
         }
-        
         return answer;
     }
 }
